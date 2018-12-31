90e454de7e3d189011a7d4abc450f4bb02524510
zakjost <jostza@amazon.com>
2018-12-31 15:03:49 -0800

    Removing random color on tags and deleting tags from single post views

--- static/js/site.js
+++ static/js/site.js
@@ -1,4 +1,4 @@
-var SemanticUIColors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
+//var SemanticUIColors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
 
 $(document).ready(function() {
   $(".ui.accordion").accordion();
@@ -6,7 +6,7 @@ $(document).ready(function() {
   $("#tag-category-pop").click(function() {
     $(".hand.point.icon").toggleClass("active");
   });
-  
+
   $('.dream-flip-toggle').click(function () {
     var target = $('.pusher')
     if (bodyBgSwitchIndex === 0) {
@@ -15,17 +15,17 @@ $(document).ready(function() {
       setTimeout( function () {
         $('.front').css('display','none')
         $('.back').css('display','block')
-      }, 300) 
+      }, 300)
     }
       else {
-        bodyBgSwitchIndex = 0      
+        bodyBgSwitchIndex = 0
         target.toggleClass('flip-it')
         setTimeout( function (){
           $('.front').css('display','block')
         $('.back').css('display','none')
       }, 300)
     }
-  })  
+  })
 });
 
 function randomInt(a, b) {
@@ -34,10 +34,13 @@ function randomInt(a, b) {
   return random = Math.floor(Math.random() * (b - a)) + a;
 }
 
+/*
 function setSemanticUIColor() {
   for (var a = [$(".dream-tags"), $(".sidebar-dream-tags")], b = 0; b < a.length; b++) {
     a[b].children().map(function() {
-      $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)]);
+      //$(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)]);
+      $(this).addClass("blue");
     });
   }
 }
+*/
